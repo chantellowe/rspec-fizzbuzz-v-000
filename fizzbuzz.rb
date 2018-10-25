9c8bf15728108f4cfb081ea141401c1831fd0dea
Chantel Lowe <chantellowe17@gmail.com>
2018-10-25 04:21:21 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -7,7 +7,7 @@ def fizzbuzz(int)
   #fizz_15 = fizzbuzz (15)
   #fizz_4 = fizzbuzz(4)
 
-  if int % 3 == 0
+  if (int % 3 == 0) && (int % 5 != 0)
     return "Fizz"
   elsif int % 5 == 0
     return "Buzz"
